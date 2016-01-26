1a86de30a4f7408fbc0ee7c2434b6b56c7d70590
R-Richard <richraponi@gmail.com>
2016-01-26 17:50:04 -0500

    BoootcampResourceCommit

---
+++ BootcampResourcesCheckoutSystem_1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BootcampResourcesCheckoutSystem_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            
+              string title = "BOOTCAMP RESOURCES CHECKOUT SYSTEM";
+                Console.SetCursorPosition((Console.WindowWidth - title.Length) / 2, Console.CursorTop);
+                Console.WriteLine(title + "\n\n\n\n\n", Console.Title);
+
+            Console.WriteLine("Main Menu");
+            Console.WriteLine("1: View Students");
+            Console.WriteLine("2: View Available Resources");
+            Console.WriteLine("3: View Student Accounts");
+            Console.WriteLine("4: Checkout Item");
+            Console.WriteLine("5: Return Item");
+            Console.WriteLine("6: Exit");
+
+
+            Console.WriteLine("\n\n\nEnter Menu Item Number");
+            int menuItem = int.Parse(Console.ReadLine());
+
+
+            bool coninueFlag = true;
+
+            while (true)
+            {
+                Console.Write("test");
+                string response = Console.ReadLine();
+                Console.Clear();
+            }
+         //   string bookArray = {"Lots Of Programming Stuff", "Even More on C#", }
+
+        }
+    }
+}
